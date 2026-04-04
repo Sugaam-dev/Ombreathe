090e8eceb2a493feba275aac4dc3f93cdbf7ccef
Bhadrayu09 <127506313+Bhadrayu09@users.noreply.github.com>
2026-04-04 15:03:56 +0530

    popup added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,6 +6,7 @@ import Footer from './Components/Footer/Footer';
 import ScrollToTop from './Components/useFullComponent/ScrollToTop';
 import FloatingDonateButton from './Components/useFullComponent/FloatingDonateButton';
 import WhatsAppButton from './Components/WhatsAppButton';
+import DiscountPopup from './Components/DiscountPopup';
 
 
 function Layout() {
@@ -43,6 +44,7 @@ function Layout() {
       />
       <ScrollToTop />
       <Navbar />
+      <DiscountPopup />  
       <main style={mainStyle}>
         <Outlet />
       </main>
